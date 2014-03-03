5f3d38c15c980d86866fa86c0856d27728c35a52
Whitney Wells <whitneyw@stanford.edu>
2014-03-03 14:15:25 -0800

    goog analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,7 +3,13 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$(".likeBtn").click(submitClicked);	
 });
+
+function submitClicked(e){
+	e.preventDefault();
+  	ga("send", "event", "like", "click");
+}
 
 /*
  * Function that is called when the document is ready.
